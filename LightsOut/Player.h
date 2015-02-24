1fc1402c6cae36aa07ce0c50b03c81ef953161b3
Kyle Nosar <kylenosar@Kyles-MacBook-Pro.local>
2015-02-24 01:41:59 -0800

    added SceneUtils class, Player class,

---
+++ LightsOut/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  LightsOut
+//
+//  Created by Kyle Nosar on 2/24/15.
+//  Copyright (c) 2015 Elijah Houle, Kyle Nosar. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Player : SKSpriteNode
+
+@end
---
+++ LightsOut/SceneUtils.m
@@ -0,0 +1,14 @@
+//
+//  SceneUtils.m
+//  LightsOut
+//
+//  Created by Kyle Nosar on 2/24/15.
+//  Copyright (c) 2015 Elijah Houle,Kyle Nosar. All rights reserved.
+//
+
+#import "SceneUtils.h"
+
+@implementation SceneUtils
+//TODO: this class will hold all attributes all scenes used need to inherit ie: controls
+
+@end
